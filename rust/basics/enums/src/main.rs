fe9c5c72f8c6b9c64ee25ed9bd0e6d5209db2cc3
SachinMulgir <smulgir636@gmail.com>
2025-11-20 01:50:48 +0530

    PREP_RUST_19112025_2: Added learnings on enum match learnings.

--- rust/basics/enums/src/main.rs
+++ rust/basics/enums/src/main.rs
@@ -1,9 +1,19 @@
 mod enums;
 mod options;
+mod matches;
 
 use crate::enums::enums;
+use crate::options::options;
+use crate::matches::matches;
 
 fn main() {
     // Enums or Enumerations:
     enums();
+
+    // Option enum : Built-in enum for Some(val) and None:
+    options();
+
+    // match : to match series of patterns and decide the actions:
+    matches();
+
 }
